e2e645c2d8d3299dbf5401e2546d350e72388ce8
Yuliya <ylobukhova@gmail.com>
2017-02-03 15:39:03 +0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+
+
+    function anchorScroll(boxAnchorLink){
+        $(boxAnchorLink + ' a').on('click', function(e){
+            e.preventDefault();
+            var attr = $(this).attr('href').substring(1);
+            var currentPosition = $(document).scrollTop();
+            var idPosition = $('#'+attr).offset().top;
+            var scrollTime = Math.abs(currentPosition - idPosition) / 3; // Math.abs - модуль числа.
+            $('body,html').animate({'scrollTop':idPosition},scrollTime);
+        });
+    };
+
+
+  anchorScroll('#to__menu');
+
+});
+
+// $(document).ready(function(){
+// $('.page-footer').after().click(function() {
+// alert('asdfahgdjkf');
+// $('html, body').animate({
+// scrollTop: 0
+// }, 1000);
+// });
+
+// });
+
+var t0, scrollTime;
+
+$('#toTop').hide().on('click', function(event){
+  event.preventDefault();
+  scrollTime = $(document).scrollTop() / 2;
+  $('body').animate({scrollTop:0}, scrollTime);
+});
+
+$(window).on('scroll', function(){
+  clearTimeout(t0);
+  t0 = setTimeout(function () {
+      showScrollTopButton();
+      console.log('scroll');
+    }, 50);
+});
+
+showScrollTopButton();
+
+function showScrollTopButton(){
+  if ( $(document).scrollTop() >= 350 ) {
+    $('#toTop').fadeIn();
+  }
+  else {
+    $('#toTop').fadeOut();
+  }
+};
